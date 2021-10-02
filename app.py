b81e38cfdfe708126f1f7402d428c734ad743195
Will Powers <williamtpowers@gmail.com>
2021-10-02 11:41:18 -0500

    added requirements.txt
    also commented out psuedo code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-
+from pytube import *
 app = Flask(__name__)
 
 
@@ -18,9 +18,23 @@ def resume():
     return render_template('resume.html')
 
 
-@app.route('/pytube')
+@app.route('/pytube', methods=['GET', 'POST'])
 def pytube():
-    return render_template('pytube.html')
+    if request.method == 'POST':
+
+        # pytube_url = # get it from web request
+        # yt = YouTube(pytube_url)
+        # title = yt.title
+        # streams = yt.streams.get_by_itag()
+        # pytube_music = yt.streams.get_audio_only()
+        # pytube_video_hq = yt.streams.get_highest_resolution()
+        # pytube_music.download()
+        # pytube_video_hq.download()
+        return render_template('pytube.html')
+
+
+    if request.method == 'GET':
+        return render_template('pytube.html')
 
 
 @app.errorhandler(404)
